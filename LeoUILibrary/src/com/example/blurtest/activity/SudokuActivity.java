60a65ad97e5aa862961513a75040a80ac03906ad
leojiang <jiangyanghaics@163.com>
2016-08-05 11:47:14 +0800

    implemented FancyIamgeLayout

--- LeoUILibrary/src/com/example/blurtest/activity/SudokuActivity.java
+++ LeoUILibrary/src/com/example/blurtest/activity/SudokuActivity.java
@@ -6,27 +6,63 @@ import android.os.Bundle;
 import com.example.blurtest.R;
 import com.example.blurtest.view.FancyImageLayout;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class SudokuActivity extends Activity {
+    private final String[] urls = {
+            "http://a2.att.hudong.com/14/06/20300001244055130863068274072.jpg",
+            "http://mg.soupingguo.com/bizhi/big/10/054/203/10054203.jpg",
+            "http://a0.att.hudong.com/51/47/01300000098168123480478377186.jpg",
+            "http://pic33.nipic.com/20131011/2531170_115542187000_2.jpg",
+            "http://pic20.nipic.com/20120406/6113596_150042568000_2.jpg",
+            "http://5.66825.com/download/pic/000/326/719d548e256e2677aeb91a7a9c464571.jpg",
+            "http://i1.17173cdn.com/9ih5jd/YWxqaGBf/forum/images/2014/08/01/231302zkvvgn1ng0lnglzn.jpg",
+            "http://www.bz55.com/uploads/allimg/101227/1_101227162638_1.jpg",
+            "http://www.impcas.ac.cn/kxcb/kxtp/201010/W020101028494020681011.jpg"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sudoku);
 
-        String[] urls = {
-//                "http://a2.att.hudong.com/14/06/20300001244055130863068274072.jpg",
-//                "http://mg.soupingguo.com/bizhi/big/10/054/203/10054203.jpg",
-//        "http://a2.att.hudong.com/14/06/20300001244055130863068274072.jpg",
-//        "http://mg.soupingguo.com/bizhi/big/10/054/203/10054203.jpg",
-        "http://a0.att.hudong.com/51/47/01300000098168123480478377186.jpg",
-        "http://pic33.nipic.com/20131011/2531170_115542187000_2.jpg",
-        "http://pic20.nipic.com/20120406/6113596_150042568000_2.jpg",
-        "http://5.66825.com/download/pic/000/326/719d548e256e2677aeb91a7a9c464571.jpg",
-        "http://i1.17173cdn.com/9ih5jd/YWxqaGBf/forum/images/2014/08/01/231302zkvvgn1ng0lnglzn.jpg",
-        "http://www.bz55.com/uploads/allimg/101227/1_101227162638_1.jpg",
-        "http://www.impcas.ac.cn/kxcb/kxtp/201010/W020101028494020681011.jpg"};
+        List<String> urlList = new LinkedList<>();
+        int count = 0;
 
-        FancyImageLayout layout = (FancyImageLayout) findViewById(R.id.imagelayout);
-        layout.setImageSources(urls);
+        FancyImageLayout layout = (FancyImageLayout) findViewById(R.id.imagelayout1);
+        urlList.add(urls[count++]);
+        layout.setImageSources(urlList.toArray());
+
+        layout = (FancyImageLayout) findViewById(R.id.imagelayout2);
+        urlList.add(urls[count++]);
+        layout.setImageSources(urlList.toArray());
+
+        layout = (FancyImageLayout) findViewById(R.id.imagelayout3);
+        urlList.add(urls[count++]);
+        layout.setImageSources(urlList.toArray());
+
+        layout = (FancyImageLayout) findViewById(R.id.imagelayout4);
+        urlList.add(urls[count++]);
+        layout.setImageSources(urlList.toArray());
+
+        layout = (FancyImageLayout) findViewById(R.id.imagelayout5);
+        urlList.add(urls[count++]);
+        layout.setImageSources(urlList.toArray());
+
+        layout = (FancyImageLayout) findViewById(R.id.imagelayout6);
+        urlList.add(urls[count++]);
+        layout.setImageSources(urlList.toArray());
+
+        layout = (FancyImageLayout) findViewById(R.id.imagelayout7);
+        urlList.add(urls[count++]);
+        layout.setImageSources(urlList.toArray());
+
+        layout = (FancyImageLayout) findViewById(R.id.imagelayout8);
+        urlList.add(urls[count++]);
+        layout.setImageSources(urlList.toArray());
+
+        layout = (FancyImageLayout) findViewById(R.id.imagelayout9);
+        urlList.add(urls[count]);
+        layout.setImageSources(urlList.toArray());
     }
 }
--- LeoUILibrary/src/com/example/blurtest/view/FancyImageLayout.java
+++ LeoUILibrary/src/com/example/blurtest/view/FancyImageLayout.java
@@ -1,54 +1,59 @@
 package com.example.blurtest.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.example.blurtest.R;
 import com.example.blurtest.giftview.BaseDensityUtil;
 import com.squareup.picasso.Picasso;
 
 /**
  * SukokuLayout is similar to a GridView that can hold at most nine children.
- * <p/>
+ *
  * Created by yanghaij on 2016/8/4.
  */
 public class FancyImageLayout extends ViewGroup {
-    private static final int ITEM_GRAP_IN_DIP = 4;
-
+    private static final int ITEM_GRAP_IN_DIP = 2;
 
     private Context mContext;
-    private String[] mImageUrls;
-
     private int mItemGap;
-    private int mWidth;
 
     /**
-     * two-dimension array, holds item's top and left
+     * two-dimension array, holds item's top and left positions
      */
     private int[][] mItemInfo;
 
     public FancyImageLayout(Context context) {
         super(context);
-        init(context);
+        init(context, null);
     }
 
     public FancyImageLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context);
+        init(context, attrs);
     }
 
     public FancyImageLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context);
+        init(context, attrs);
     }
 
-    private void init(Context context) {
+    private void init(Context context, AttributeSet attrs) {
         mContext = context;
         mItemGap = BaseDensityUtil.dip2px(context, ITEM_GRAP_IN_DIP);
         setBackgroundColor(Color.WHITE);
+
+        if (attrs != null) {
+            TypedArray typedArr = context.obtainStyledAttributes(attrs,
+                    R.styleable.FancyImageLayout);
+            mItemGap = (int) typedArr.getDimension(R.styleable.FancyImageLayout_item_gap, ITEM_GRAP_IN_DIP);
+            typedArr.recycle();
+        }
     }
 
     @Override
@@ -62,15 +67,15 @@ public class FancyImageLayout extends ViewGroup {
     /**
      * calculate each item's size, and return the height of FancyImageLayout.
      *
-     * @param totalWidth
-     * @return
+     * @param totalWidth width
+     * @return height
      */
     private int doMeasurement(int totalWidth) {
-        if (mImageUrls == null || mImageUrls.length <= 0) {
+        int count = getChildCount();
+        if (count <= 0) {
             return 0;
         }
 
-        int count = mImageUrls.length;
         mItemInfo = new int[count][4];
 
         switch (count) {
@@ -81,15 +86,57 @@ public class FancyImageLayout extends ViewGroup {
             case 4:
                 return measureItems5Excluded(totalWidth, 2);
             case 6:
-                return measure6Item(totalWidth);
+                return measureItemsFirstBigger(totalWidth, 3, 2);
+            case 13:
+                return measureItemsFirstBigger(totalWidth, 4, 2);
             default:
                 return measureItems5Excluded(totalWidth, 3);
         }
     }
 
+    /**
+     * The first item it multi times size of normal items.
+     * Be careful when using this method, cause there're several requirements
+     * should be fullfilled.
+     *
+     * @param totalWidth the width of FancyImageLayout
+     * @param column     column numbers of normal items
+     * @param multiple   times of the first item's size compares to normal items
+     * @return height of FancyImageLayout
+     */
+    private int measureItemsFirstBigger(int totalWidth, int column, int multiple) {
+        if (multiple >= column) {
+            return 0;
+        }
 
-    private int measure6Item(int totalWidth) {
-        return 0;
+        int count = getChildCount();
+        int normalItemSize = (totalWidth - mItemGap * (column - 1)) / column;
+        int fistItemSize = normalItemSize * multiple + mItemGap * (multiple - 1);
+        int rightTopPartColumn = column - multiple;
+        for (int i = 0; i < count; i++) {
+            if (i == 0) {
+                //the first one is bigger than others
+                mItemInfo[i][0] = 0;
+                mItemInfo[i][1] = 0;
+                mItemInfo[i][2] = fistItemSize;
+                mItemInfo[i][3] = fistItemSize;
+            } else {
+                if (i <= rightTopPartColumn * multiple) {
+                    //right-top area
+                    mItemInfo[i][0] = fistItemSize + mItemGap + (normalItemSize + mItemGap) * ((i - 1) % rightTopPartColumn);
+                    mItemInfo[i][1] = (normalItemSize + mItemGap) * ((i - 1) / rightTopPartColumn);
+                } else {
+                    //bottom area
+                    int subIndex = i - rightTopPartColumn * multiple - 1;
+                    mItemInfo[i][0] = (normalItemSize + mItemGap) * (subIndex % column);
+                    mItemInfo[i][1] = fistItemSize + mItemGap + (normalItemSize + mItemGap) * (subIndex / column);
+                }
+                mItemInfo[i][2] = mItemInfo[i][0] + normalItemSize;
+                mItemInfo[i][3] = mItemInfo[i][1] + normalItemSize;
+            }
+        }
+
+        return mItemInfo[count - 1][3];
     }
 
     private static final int SIZE_RATIO = 2;
@@ -102,8 +149,8 @@ public class FancyImageLayout extends ViewGroup {
         for (int i = 0; i < count; i++) {
             View child = getChildAt(i);
 
-            //calculate item size of items in first line
             if (i < firstLineNumb) {
+                //calculate item size of items in first line
                 int itemWidth = (totalWidth - mItemGap * (firstLineNumb - 1)) / firstLineNumb;
                 int itemHeight = itemWidth;
 
@@ -112,7 +159,7 @@ public class FancyImageLayout extends ViewGroup {
                     itemHeight = itemWidth / SIZE_RATIO; //or itemWidth / (colunm - 1)
                 }
 
-                //save height for first line item, added to other lines top value.
+                //save height of first line item.
                 firstLineHeight = itemHeight;
 
                 int measureSpecWidth = MeasureSpec.makeMeasureSpec(itemWidth, MeasureSpec.EXACTLY);
@@ -127,8 +174,8 @@ public class FancyImageLayout extends ViewGroup {
                 child.measure(makeMeasureSpec, makeMeasureSpec);
 
                 mItemInfo[i][0] = (itemMaxSize + mItemGap) * ((i - firstLineNumb) % column);
-                int top = firstLineHeight + (firstLineHeight > 0 ? mItemGap : 0) + (i - firstLineNumb) / column * (itemMaxSize + mItemGap);
-                mItemInfo[i][1] = top;
+                mItemInfo[i][1] = firstLineHeight + (firstLineHeight > 0 ? mItemGap : 0) +
+                        (i - firstLineNumb) / column * (itemMaxSize + mItemGap);
             }
 
             mItemInfo[i][2] = mItemInfo[i][0] + child.getMeasuredWidth();
@@ -146,24 +193,25 @@ public class FancyImageLayout extends ViewGroup {
         }
 
         for (int i = 0; i < getChildCount(); i++) {
-            final View child = getChildAt(i);
+            getChildAt(i).layout(mItemInfo[i][0], mItemInfo[i][1], mItemInfo[i][2], mItemInfo[i][3]);
+        }
+    }
 
-            if (child.getVisibility() == GONE) {
-                continue;
-            }
+    public void setImageSources(Object[] urls) {
+        mItemInfo = new int[urls.length][4];
 
-            child.layout(mItemInfo[i][0], mItemInfo[i][1], mItemInfo[i][2], mItemInfo[i][3]);
+        for (Object url : urls) {
+            addImageView((String)url);
         }
     }
 
     public void setImageSources(String[] urls) {
-        mImageUrls = urls;
-        mItemInfo = new int[mImageUrls.length][4];
+        mItemInfo = new int[urls.length][4];
 
-
-        for (int i = 0; i < mImageUrls.length; i++) {
-            addImageView(mImageUrls[i]);
+        for (String url : urls) {
+            addImageView(url);
         }
+
     }
 
     private void addImageView(String url) {
